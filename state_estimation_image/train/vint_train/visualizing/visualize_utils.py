942f56caa09c18e8d42df75927b375e5215e708f
Wan Ziyu <142478860+ZiyuNUS@users.noreply.github.com>
2025-03-11 20:54:30 +0800

    Delete state_estimation_image/train/vint_train directory

--- state_estimation_image/train/vint_train/visualizing/visualize_utils.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import torch
-
-def to_numpy(tensor: torch.Tensor) -> np.ndarray:
-    return tensor.detach().cpu().numpy()
-
-def from_numpy(array: np.ndarray) -> torch.Tensor:
-    return torch.from_numpy(array).float()
